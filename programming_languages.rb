50d3f477f4bd8a50165ee29abc5a7dbc3d9a6f27
Ernie Val <erndval@gmail.com>
2019-03-21 21:26:35 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  style_array = []
+  languages.each do |style_sym, style_hash|
+    style_hash.each do |lang_sym, lang_hash|
+      lang_hash.each do |type_sym, type_str|
+        if lang_sym == :javascript
+          style_array << style_sym
+          new_hash[lang_sym] = {type_sym => type_str, style: style_array}
+        else
+          new_hash[lang_sym] = {type_sym => type_str, style: [style_sym]}
+        end
+      end
+    end
+  end
+  new_hash
 end
