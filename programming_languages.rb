4ea83d38424e7df6ddeaaa884a7ff299ccf61c8c
Ernie Val <erndval@gmail.com>
2019-03-21 21:03:44 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,6 +3,8 @@ require "pry"
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, style_hash|
-    binding.pry
+    style_hash.each do |lang_sym, lang|
+      binding.pry
+    end
   end
 end
